8a6e22a5e51db620d530db74b9d94404597bd8e8
Huzefa <hafatakd@uwaterloo.ca>
2014-03-02 13:51:59 -0500

    PR #4: Changed button ids to make them more meaningful.

--- whack/src/com/turbo/whack/FullscreenActivity.java
+++ whack/src/com/turbo/whack/FullscreenActivity.java
@@ -8,13 +8,6 @@ import android.widget.Button;
 
 // Full screen activity
 public class FullscreenActivity extends Activity {
-	
-// HF: I don't even know if we need these definitions...? I've left this just in case.
-//	private static final boolean AUTO_HIDE = true;				// Hide system UI
-//	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;		// Time in ms before the system UI is hidden
-//	private static final boolean TOGGLE_ON_CLICK = true;		// Toggle system UI on interaction
-//	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;	// Hide nav bar in api 19+
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		ActivityHelper.activity_init(this);
