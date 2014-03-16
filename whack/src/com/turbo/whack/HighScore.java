19aca198c7d8514e948b4b0fc9403ec4d2ae64b1
Huzefa <hafatakd@uwaterloo.ca>
2014-03-16 15:18:46 -0400

    PR #11: Can now write the highscore in the correct place.

--- whack/src/com/turbo/whack/HighScore.java
+++ whack/src/com/turbo/whack/HighScore.java
@@ -13,6 +13,10 @@ public class HighScore {
 	private SimpleDataStore sd;
 	private JSONObject jObj;
 	
+	/**
+	 * Highscore object constructor.
+	 * @throws JSONException Non recoverable.
+	 */
 	public HighScore() throws JSONException {
 		sd = new SimpleDataStore(ActivityHelper.WH_APP_NAME);
 		String data = sd.retrieve_string_value(ActivityHelper.WH_DATA_NAME);
@@ -29,8 +33,13 @@ public class HighScore {
 		}
 	};
 	
-	// hs_check()
-	
+	/**
+	 * This function serializes the data to work with JSON and modifies the JSON file accordingly.
+	 * @param name The name associated with the highscore. Cannot be null.
+	 * @param score The score associated with the highscore. Must be greater than zero.
+	 * @return Zero on success. Non-zero on failure.
+	 * @throws JSONException
+	 */
 	public int hs_serialize(String name, int score) throws JSONException {
 		if(name == null || score <= 0) {
 			Log.i(ActivityHelper.WH_LOG_INFO, "Invalid name or score.");
@@ -56,11 +65,17 @@ public class HighScore {
 	
 	// hs_deserialize();
 	
+	/**
+	 * A function that gets the record holding the lowest score when compared to the current score.
+	 * @param score The current score
+	 * @return A string that is the record name. Null if not a highscore.
+	 * @throws JSONException If we catch this, there is very little possibility of recovery.
+	 */
 	@SuppressWarnings("unchecked")
 	private String hs_get_lowest(int score) throws JSONException {
 		boolean is_empty = true;
 		boolean score_exists = false;
-		final String record = "record";
+		final String record = null;
 		
 		// Check if empty or exists
 		for(int i = 1; i <= 10; i++) {
@@ -81,18 +96,56 @@ public class HighScore {
 			return "record1";
 		}
 		
-		
-		
-		// This cannot be null.
+		// Figure out where to put it and adjust.
+		for(int i = 1; i <= 10; i++) {
+			String rec = "record" + i;
+			int s = hs_get_score(rec);
+			if(s < 0) {
+				return rec;
+			}
+			if(i < score) {
+				hs_push_down(i);
+				return rec;
+			}
+		}
+		// The score is not a high score.
 		return null;
 	}
 	
+	/**
+	 * Pushes down all the scores by 1 to make room for another.
+	 * @param rank The rank where you want to insert.
+	 * @return zero on success
+	 * @throws JSONException Non-recoverable.
+	 */
+	@SuppressWarnings("unchecked")
+	private int hs_push_down(int rank) {
+		Map<String, String> tmp;
+		String r;
+		try {
+			for(int i = 10; i >= rank; i--) {
+				r = "record" + (i - 1);
+				tmp = (HashMap<String, String>) jObj.get(r);
+				jObj.put("record" + i, tmp);
+			}		
+			jObj.put("record" + rank, null);
+		} catch(JSONException j) {
+			return -1;
+		}
+		return 0;
+	}
+	
+	/**
+	 * A private function that gets the score from a record.
+	 * @param record
+	 * @return An integer that represents the score. -1 on failure.
+	 * @throws JSONException
+	 */
 	@SuppressWarnings("unchecked")
 	private int hs_get_score(String record) throws JSONException {
 		int res;
-		
-		// HF: If the type cast fails, we're done for.
 		Map<String, String> map = (HashMap<String, String>) jObj.get(record);
+		
 		try {
 			res = Integer.parseInt(map.get("score"));
 		} catch(NumberFormatException e) {
@@ -101,11 +154,20 @@ public class HighScore {
 		return res;
 	}
 	
+	/**
+	 * A API that returns a sorted array of size 10 with the highscores.
+	 * @return An array of type Map<String, String>[] where the strings are name and score respectively
+	 */
 	public Map<String, String>[] hs_get_all() {
 		return null;	
 	}
 	
-	public boolean close() {
+	/**
+	 * A function that must be called to save the highscores to memory to be loaded next time.
+	 * Make sure that you call this function before losing the object or there is no guarantee that
+	 * the scores will be recorded.
+	 */
+	public boolean hs_close() {
 		String s = jObj.toString();
 		if(s == null) {
 			Log.i(ActivityHelper.WH_LOG_ERRO, "String conversion failed.");
