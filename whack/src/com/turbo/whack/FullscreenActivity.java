9319601490040845735ebbdc22352ff6da4611aa
dpatelloo <dp-1994@hotmail.com>
2014-03-17 22:46:23 -0400

    PR #4: Switch buttons on state change (test on instruction button on main screen)

--- whack/src/com/turbo/whack/FullscreenActivity.java
+++ whack/src/com/turbo/whack/FullscreenActivity.java
@@ -3,9 +3,12 @@ package com.turbo.whack;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 // Full screen activity
 public class FullscreenActivity extends Activity {
@@ -19,7 +22,7 @@ public class FullscreenActivity extends Activity {
 		
 		Button start_button = (Button) findViewById(R.id.start_button);
 		Button instr_button = (Button) findViewById(R.id.instructions_button);
-		
+		Button high_score = (Button) findViewById(R.id.high_score_button);
 		start_button.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
@@ -31,8 +34,21 @@ public class FullscreenActivity extends Activity {
 		instr_button.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// TODO something with instr button
-				
+				Log.d("MSG", "Instruction Button Pressed");			
+			}
+		});
+		instr_button.setOnFocusChangeListener(new OnFocusChangeListener() {
+
+			@Override
+			public void onFocusChange(View arg0, boolean arg1) {
+				Log.d("MSG", "Instruction Button Pressed: " + arg1);	
+			}
+		});
+		
+		high_score.setOnClickListener(new View.OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(FullscreenActivity.this, HighScoreActivity.class));				
 			}
 		});
 	}
